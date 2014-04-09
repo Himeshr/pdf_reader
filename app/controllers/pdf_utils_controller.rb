877617e16c253683480b4de8a221f36cf2ae9773
webwerks <webwerks@webwerks-G31M-ES2L.(none)>
2014-04-09 18:25:43 +0530

    added comments for methods.

--- app/controllers/pdf_utils_controller.rb
+++ app/controllers/pdf_utils_controller.rb
@@ -1,8 +1,10 @@
 class PdfUtilsController < ApplicationController
 
+  # GET pdf_utils/new
   def new
   end
 
+  # POST pdf_utils/process_pdf
   def process_pdf
     puts params.inspect
     @data = PdfUtil.process(params[:pdf])
--- app/models/pdf_util.rb
+++ app/models/pdf_util.rb
@@ -2,8 +2,12 @@ class PdfUtil
   DATE_REGEX = '(([012][0-9])|([3][01]))\/(([0][1-9])|([1][012]))\/([\d]{4})'
 
   class << self
+    # This method accept the pdf file object and extract the require data.
+    # parameters : file object of pdf file.
+    # Return Types : hash with reuire data.
     def process(file)
       file = file.tempfile
+      # Read pdf file.
       pdf_text = RPDFBox::TextExtraction.get_text_all(file.path)
       data = {}
       company_and_client_name(data, pdf_text)
@@ -12,17 +16,27 @@ class PdfUtil
       data
     end
 
+    # This method extract the contact number from given string.
+    # Contact number formate should be (123 123 1234, 123-123-1234)
+    # Parametrs: hash object and string.
+    # Return Type : contact number is added in hash.
     def contact_no(data, pdf_text)
       txt = pdf_text.match(/(?=VAT\sREG)[^>]+((?<=(([012][0-9])|([3][01]))\/(([0][1-9])|([1][012]))\/([\d]{4})))/m)[0]
       data[:contact_no] = txt.match(/([\d]{3}(\s|-)[\d]{3}(\s|-)[\d]{4})/m)[0]
     end
 
+    # This method extract the company name and client name from given string.
+    # Parametrs: hash object and string.
+    # Return Type : client name and company name is hash.
     def company_and_client_name(data, pdf_text)
       company_client_name = pdf_text.match(/(?<=(([012][0-9])|([3][01]))\/(([0][1-9])|([1][012]))\/([\d]{4}))(.)*(?=P O BOX)/m)[0]
       data[:client_name] = company_client_name.split("\n")[-1]
       data[:company_name] = company_client_name.split("\n")[-2]
     end
 
+    # This method ectract the table bill data from given string.
+    # Parameter: hash and string.
+    # Return Type : added bill data in hash.
     def bill_charges(data, pdf_text)
       bill_data = pdf_text.match(/(?<=DATE TRANSACTION AMOUNT)(.)*(?=TOTAL EXCLUDING VAT)/m)[0]
       bill_data = bill_data.split("\n").reject{|i| i.blank? }
@@ -30,6 +44,9 @@ class PdfUtil
       #
     end
 
+    # This method extract the row data from string.
+    # Parameter : hash and string
+    # Return Type : added bill data in hash.
     def process_bill_data(data, bill_data)
       bill_charges = []
       bill_data.each do |b|
@@ -37,7 +54,6 @@ class PdfUtil
         bill_charges << [table_row[1], table_row[5], table_row[6]]
       end
       data[:bill_charges] = bill_charges
-
     end
   end
 
